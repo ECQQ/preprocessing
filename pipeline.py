cdfd05b8f252ce8c65e4e11d2fb6ed4588a833a1
Cristobal <cridonoso@inf.udec.cl>
2021-02-10 17:31:12 -0300

    distributed version

--- pipeline.py
+++ pipeline.py
@@ -25,8 +25,7 @@ def process_one(row, k):
             single_col = tt.check_spelling(single_col)
             single_col = tt.equivalent_words(single_col)                    
             
-            if single_col.shape[0] < 1: continue
-            emotions = single_col
+            emo_serie = single_col[(~pd.isna(single_col)) & (single_col != 'nan')]
             person_id = re.search(r'\d+', m).group() 
             user_id = '{}_{}'.format(k, person_id)
 
@@ -35,35 +34,33 @@ def process_one(row, k):
             sex = row['LP_SEXO{}'.format(person_id)]
             level = row['LP_NIVEL{}'.format(person_id)]
             group = row['Grupo']
-            id_file = row['ID Archivo']
-            
-            emo_serie = emotions[(~pd.isna(emotions)) & (emotions != 'nan')]
-
-
+            id_file = row['ID Archivo']        
             emo_serie = emo_serie.reset_index()
-            idu_serie = pd.Series([user_id]*len(emo_serie))
-            age_serie = pd.Series([age]*len(emo_serie))
-            sex_serie = pd.Series([sex]*len(emo_serie))
-            lev_serie = pd.Series([level]*len(emo_serie))
-            grp_serie = pd.Series([group]*len(emo_serie))
-            idf_serie = pd.Series([id_file]*len(emo_serie))
-            
-            final = pd.concat([idf_serie, 
-                                idu_serie,
-                                grp_serie, 
-                                age_serie, 
-                                lev_serie, 
-                                sex_serie, 
-                                emo_serie], 1)
-            
-            final.columns = ['id_file', 'id_user', 'group', 'age', 'education', 'sex', 'priority', 'emotion']
-            finals_df.append(final)
-    try:
-        finals_df = pd.concat(finals_df)
-    except:
-        pass
 
-    return finals_df
+            if emo_serie.shape[0] > 1:
+                idu_serie = pd.Series([user_id]*len(emo_serie))
+                age_serie = pd.Series([age]*len(emo_serie))
+                sex_serie = pd.Series([sex]*len(emo_serie))
+                lev_serie = pd.Series([level]*len(emo_serie))
+                grp_serie = pd.Series([group]*len(emo_serie))
+                idf_serie = pd.Series([id_file]*len(emo_serie))
+                
+                final = pd.concat([idf_serie, 
+                                    idu_serie,
+                                    grp_serie, 
+                                    age_serie, 
+                                    lev_serie, 
+                                    sex_serie, 
+                                    emo_serie], 1)
+
+                final.columns = ['id_file', 'id_user', 'group', 'age', 'education', 'sex', 'priority', 'emotion']
+                finals_df.append(final)
+
+    if len(finals_df)>1:
+        finals_df = pd.concat(finals_df)
+        return finals_df
+    else:
+        return None
     
 def run_pipeline():
 
@@ -74,7 +71,7 @@ def run_pipeline():
     # Reading data
     comunas = pd.read_csv('./datos/comuna_region.csv', usecols=[0])
     if opt.debug:
-        chunksize = 50
+        chunksize = 100
         dialogos_df = pd.read_csv(opt.data, chunksize=chunksize, low_memory=False)
         for frame in dialogos_df: break
     else:
@@ -85,7 +82,7 @@ def run_pipeline():
     allframes = Parallel(n_jobs=num_cores)(delayed(process_one)(row, k) \
                     for k, row in frame.iterrows())
 
-    allframes = [f for f in allframes if f != []]
+    allframes = [f for f in allframes if f is not None]
     new_data = pd.concat(allframes, 0)
     new_data.to_csv('./datos/Dialogo/emo_per_user.csv', index=False)
 
