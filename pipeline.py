88c7a84046a68111b9ed38878d6faf3bc3b35450
Cristobal <cridonoso@inf.udec.cl>
2021-02-10 15:49:16 -0300

    distributed

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,17 @@
+import multiprocessing
 import textools as tt
 import pandas as pd
 import argparse
 import re
 
+from joblib import Parallel, delayed
+
+
 def run_pipeline():
 
     # Init Pipeline
-    print('[INFO] file: {}'.format(opt.data))
+    num_cores = multiprocessing.cpu_count()
+    print('[INFO] file: {} - {} CPU'.format(opt.data, num_cores))
 
     # Reading data
     comunas = pd.read_csv('./datos/comuna_region.csv', usecols=[0])
@@ -37,16 +42,19 @@ def run_pipeline():
 
     # Separate groups
     re_1 = r'LP_EDAD'
-    re_2 = r'\d+'
+
     member_cols = [x for x in frame.columns if re.search(re_1, x)]
     allframes = []
     user_id = 0
-    for k, row in frame.iterrows():
+
+    def fn(member_cols, row, k):
+        finals = []
         for m in member_cols:
             age = row[m]
             if not pd.isna(age):
                 emotions = row[frame_cols]
-                person_id = re.search(re_2, m).group() 
+                person_id = re.search(r'\d+', m).group() 
+                user_id = '{}_{}'.format(k, person_id)
 
                 comuna = row['LP_COMUNA{}'.format(person_id)]
                 comuna = row['Comuna'] if pd.isna(comuna) else comuna 
@@ -65,18 +73,24 @@ def run_pipeline():
                 idf_serie = pd.Series([id_file]*len(emo_serie))
                 
                 final = pd.concat([idf_serie, 
-                                   idu_serie,
-                                   grp_serie, 
-                                   age_serie, 
-                                   lev_serie, 
-                                   sex_serie, 
-                                   emo_serie], 1)
+                                    idu_serie,
+                                    grp_serie, 
+                                    age_serie, 
+                                    lev_serie, 
+                                    sex_serie, 
+                                    emo_serie], 1)
                 
                 final.columns = ['id_file', 'id_user', 'group', 'age', 'education', 'sex', 'priority', 'emotion']
-                user_id += 1
-                allframes.append(final)   
+                finals.append(final)
 
-    new_data = pd.concat(allframes, 0)
+        return finals  
+
+    allframes = Parallel(n_jobs=num_cores)(delayed(fn)(member_cols, row, k) \
+                        for k, row in frame.iterrows())
+    newframe = []
+    for f in allframes:
+        newframe+=f
+    new_data = pd.concat(newframe, 0)
     new_data.to_csv('./datos/Dialogo/emo_per_user.csv', index=False)
 
 if __name__ == '__main__':
